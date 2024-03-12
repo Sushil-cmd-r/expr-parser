f3d870606c1fd69afb04936fc3cedb9ca86905a2
sushil-cmd-r <kandharesushil10@gmail.com>
2024-03-13 01:20:20 +0530

    Added Scanner

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/sushil-cmd-r/expr-parser/scanner"
+	"github.com/sushil-cmd-r/expr-parser/token"
+)
+
+const Propmt = "> "
 
 func main() {
-	fmt.Println("Expression Parser")
+	fmt.Println("Welcome to Expression Parser!")
+
+	bufsc := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(Propmt)
+		scanned := bufsc.Scan()
+		if !scanned {
+			break
+		}
+
+		line := bufsc.Text()
+		if line == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+		if line == "" {
+			continue
+		}
+
+		lexer := scanner.New([]byte(line))
+
+		for tok := lexer.Next(); tok.Type != token.Eof; tok = lexer.Next() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
 }
---
+++ scanner/scanner.go
@@ -0,0 +1,79 @@
+package scanner
+
+import (
+	"fmt"
+
+	"github.com/sushil-cmd-r/expr-parser/token"
+)
+
+type Scanner struct {
+	source   []byte
+	rdOffset int
+}
+
+func New(source []byte) *Scanner {
+	return &Scanner{
+		source:   source,
+		rdOffset: 0,
+	}
+}
+
+func (s *Scanner) Next() token.Token {
+	for !s.isAtEnd() {
+		ch := s.advance()
+		switch ch {
+		// Scan Operators
+		case '+':
+			return token.New(token.Plus, "+")
+		case '-':
+			return token.New(token.Minus, "-")
+		case '*':
+			return token.New(token.Star, "*")
+		case '/':
+			return token.New(token.Slash, "/")
+			// Ignore Whitespaces
+		case '\n', ' ', '\t', '\r':
+		// Numbers
+		default:
+			if isNum(ch) {
+				return s.scanNumber()
+			}
+			return token.New(token.Illegal, fmt.Sprintf("unknown token: %s.", string(ch)))
+
+		}
+	}
+	return token.New(token.Eof, "Eof")
+}
+
+func (s *Scanner) scanNumber() token.Token {
+	st := s.rdOffset - 1
+	for isNum(s.peek()) {
+		s.advance()
+	}
+	lit := s.source[st:s.rdOffset]
+
+	return token.New(token.Number, string(lit))
+}
+
+func (s *Scanner) advance() byte {
+	if s.isAtEnd() {
+		return ' '
+	}
+	s.rdOffset += 1
+	return s.source[s.rdOffset-1]
+}
+
+func (s *Scanner) peek() byte {
+	if s.isAtEnd() {
+		return ' '
+	}
+	return s.source[s.rdOffset]
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.rdOffset >= len(s.source)
+}
+
+func isNum(ch byte) bool {
+	return ch >= '0' && ch <= '9'
+}

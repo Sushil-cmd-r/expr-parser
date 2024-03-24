fe508ccc5db86ac3679cbd0bc4e840e40b89c086
sushil-cmd-r <kandharesushil10@gmail.com>
2024-03-24 14:37:15 +0530

    Extract repl from main.go

--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-
-	"github.com/sushil-cmd-r/expr-parser/parser"
-	"github.com/sushil-cmd-r/expr-parser/scanner"
-)
-
-const Propmt = "> "
+import "github.com/sushil-cmd-r/expr-parser/repl"
 
 func main() {
-	fmt.Println("Welcome to Expression Parser!")
-
-	bufsc := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print(Propmt)
-		scanned := bufsc.Scan()
-		if !scanned {
-			break
-		}
-
-		line := bufsc.Text()
-		if line == "exit" {
-			fmt.Println("Exiting...")
-			break
-		}
-		if line == "" {
-			continue
-		}
-
-		sc := scanner.New([]byte(line))
-		p := parser.New(sc)
-		expr := p.Parse()
-		if expr != nil {
-			fmt.Println(expr)
-		}
-	}
+	repl.Start()
 }
---
+++ repl/repl.go
@@ -0,0 +1,43 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/sushil-cmd-r/expr-parser/parser"
+	"github.com/sushil-cmd-r/expr-parser/scanner"
+)
+
+const Propmt = "> "
+
+func Start() {
+	fmt.Println("Welcome to Expression Parser!")
+
+	bufsc := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(Propmt)
+		scanned := bufsc.Scan()
+		if !scanned {
+			break
+		}
+
+		line := bufsc.Text()
+		if line == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+		if line == "" {
+			continue
+		}
+
+		sc := scanner.New([]byte(line))
+		p := parser.New(sc)
+		expr, err := p.Parse()
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(expr)
+		}
+	}
+}
